7b519a2314b87adf6773a7a915497a42413586ef
qxdo <qiaoxindong@kilofolo.com>
2024-04-28 11:15:00 +0800

    update code

--- api/index.go
+++ api/index.go
@@ -1,10 +1,22 @@
-package handler
- 
+package api
+
 import (
-  "fmt"
-  "net/http"
+	"github.com/labstack/echo/v4"
+	"net/http"
 )
- 
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
+var srv http.Handler
+
+func init() {
+	e := echo.New()
+	e.GET("/books", Test)
+	srv = e
+}
+
+func Test(echo.Context) error {
+	return nil
+}
+
+func MainFunc(w http.ResponseWriter, r *http.Request) {
+	srv.ServeHTTP(w, r)
 }
